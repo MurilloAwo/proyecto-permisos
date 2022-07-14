b26a7e22644acc6439b10e029c0bc0430807ed0f
Hector <hector.1701710051@ucaldas.edu.co>
2022-07-14 12:18:58 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import  Menu  from './components/nav/Menu'
+import { Row, Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <Container>
-  <Row>
-    
-  </Row>
-</Container>
-   
+    <Container fluid>
+      <Row>
+        <Menu></Menu>
+      </Row>
+    </Container>
+
   );
 }
 
---
+++ src/components/permisos/VerPermisos.js
@@ -0,0 +1,10 @@
+const Permiso = () => {
+
+    return (
+        <div>
+            <h1>holaa</h1>
+        </div>
+    )
+
+}
+export default Permiso;
